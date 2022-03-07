111920d35a9d2f521e09323f04bb0cf3cc9fdd74
Edmilson Torres <edmilson.torres@gmail.com>
2022-03-06 21:21:35 -0300

    feat: photo listing names

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import { useState, useEffect } from "react";
 import * as C from "./App.styles";
+import * as Photos from "./services/photos";
+import { Photo } from "./types/Photos";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
+  const getPhotos = async () => {
+    setLoading(true);
+    setPhotos(await Photos.getAll());
+    setLoading(false);
+  };
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Galeria de Fotos</C.Header>
+        {loading && (
+          <C.ScreenWarning>
+            <div className="emoji">🤚</div>
+            <div>Carregando...</div>
+          </C.ScreenWarning>
+        )}
+
+        {!loading && photos.length > 0 && (
+          <C.PhotoList>
+            {photos.map((item, index) => (
+              <div>{item.name}</div>
+            ))}
+          </C.PhotoList>
+        )}
       </C.Area>
     </C.Container>
   );
